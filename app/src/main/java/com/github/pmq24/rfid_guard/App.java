8952fc0b6cc8b52b3faa0817134f35062e89c754
Quang Pham <qquang24t5@gmail.com>
2022-03-25 17:29:46 +0700

    Add MainWindow.java

--- app/src/main/java/com/github/pmq24/rfid_guard/App.java
+++ app/src/main/java/com/github/pmq24/rfid_guard/App.java
@@ -4,6 +4,7 @@ package com.github.pmq24.rfid_guard;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setVisible(true);
     }
 }
