2ee343d3c7fc6c76db65c603f811281293a01a0e
Laro-svg <72305053+Laro-svg@users.noreply.github.com>
2022-05-07 11:53:28 +0700

    Update TagReader.java

--- app/src/main/java/com/github/pmq24/rfid_guard/reading/TagReader.java
+++ app/src/main/java/com/github/pmq24/rfid_guard/reading/TagReader.java
@@ -4,15 +4,11 @@ import com.github.pmq24.rfid_guard.data.TagRead;
 import com.impinj.octane.OctaneSdkException;
 
 public abstract class TagReader {
-// hàm chạy thiết bị
     public abstract void start() throws OctaneSdkException;
-// hàm ngưng thiết bị
     public abstract void stop() throws OctaneSdkException;
-// hàm setTagReadListener sẽ được gọi khi có tag đi qua 
     public void setTagReadListener(TagReadListener listener) {
         tagReadListener = listener;
     }
-// hàm notifyListener thông báo tag đi qua thiết bị 
     protected void notifyListener(TagRead tagRead) {
         tagReadListener.onRead(tagRead);
     }
